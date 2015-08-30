eea932d218bd1b511095dbb8a908d0159933662f
Andreas Blaesius <skate4life@gmx.de>
2015-08-30 21:15:13 +0200

    DeviceSettings: add an option to enable Samsung Keyboard dock
    Our sec_keyboard driver does not properly unregister the input device when no
    dock is detected, which makes the system believe that a physical keyboard is
    present at all times, which prevents the software keyboard from being shown.
    
    Some user own a Keyboard-Dock and meight miss the ability to use the dock if
    it gets disabled completely.
    It's build as module to give the user the ability to load the driver manually.
    
    Change-Id: Ie351f18b996972595e9198481bd568bdd8f8adc6

---
+++ DeviceSettings/src/com/cyanogenmod/settings/device/KernelModule.java
@@ -0,0 +1,67 @@
+package com.cyanogenmod.settings.device;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.content.Context;
+import android.view.*;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import java.io.File;
+
+import com.cyanogenmod.settings.device.R;
+
+public class KernelModule extends Fragment {
+
+    public static String TAG = "Module Switcher";
+    private static final String SEC_DOC_KEYBOARD = "sec_dock_keyboard";
+    public static final String SEC_DOC_KEYBOARD_PATH = "/system/lib/modules/sec_dock_keyboard.ko";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup root,
+            Bundle savedInstanceState) {
+        super.onCreateView(inflater, root, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.kernel_module, root, false);
+
+        if (!new File(SEC_DOC_KEYBOARD_PATH).exists()) {
+            Toast.makeText(getActivity(), SEC_DOC_KEYBOARD + " module does not exist", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        SU su = new SU();
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox);
+        checkBox.setChecked(isModuleActive(SEC_DOC_KEYBOARD, su));
+        su.close();
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SU su = new SU();
+                if (isChecked) insmod(SEC_DOC_KEYBOARD_PATH, su);
+                else rmmod(SEC_DOC_KEYBOARD, su);
+                su.close();
+                saveConfig(isChecked);
+            }
+        });
+        return view;
+    }
+
+    private boolean isModuleActive(String module, SU su) {
+        return su.runCommand("lsmod | grep module").contains(module);
+    }
+
+    private void insmod(String module, SU su) {
+        su.runCommand("insmod " + module);
+    }
+
+    private void rmmod(String module, SU su) {
+        su.runCommand("rmmod " + module);
+    }
+
+    private void saveConfig(boolean active) {
+        getSharedPreferences("prefs", Context.MODE_PRIVATE).edit().putBoolean("moduleactive", active).apply();
+    }
+
+}
